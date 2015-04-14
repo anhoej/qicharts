7a5cbfea0c4d76838aa311f056feac2c3001779b
roeder <roeder.t17@gmail.com>
2015-04-14 21:08:21 +0200

    first steps cusum

---
+++ R/cusumchart.R
@@ -0,0 +1,50 @@
+cusumchart <- function(y,
+                       x,
+                       target,
+                       sdev,
+                       shift = NULL,
+                       k     = 0.5,
+                       h     = 5) {
+
+}
+
+
+
+# cusumchart1 <- function(y,
+#                        x,
+#                        data,
+#                        notes        = NULL,
+#                        cl           = NULL,
+#                        ylim         = NULL,
+#                        target       = NULL,
+#                        freeze       = NULL,
+#                        breaks       = NULL,
+#                        exclude      = NULL,
+#                        negy         = TRUE,
+#                        dots.only    = FALSE,
+#                        multiply     = 1,
+#                        x.format     = '%Y-%m-%d',
+#                        cex          = 0.8,
+#                        main,
+#                        xlab         = 'Subgroup',
+#                        ylab         = 'Indicator',
+#                        decimals     = NULL,
+#                        pre.text     = 'Before data',
+#                        post.text    = 'After data',
+#                        runvals      = FALSE,
+#                        linevals     = TRUE,
+#                        plot.chart   = TRUE,
+#                        prnt         = FALSE,
+#                        primed       = FALSE,
+#                        standardised = FALSE,
+#                        ...) {
+#   # Get data, sample sizes, subgroups, and notes
+#   if(!missing(data)){
+#     class(data) <- 'data.frame'
+#     y <- data[,deparse(substitute(y))]
+#     if(deparse(substitute(x)) %in% colnames(data))
+#       x <- data[,deparse(substitute(x))]
+#     if(deparse(substitute(notes)) %in% colnames(data))
+#       notes <- data[,deparse(substitute(notes))]
+#   }
+# }
